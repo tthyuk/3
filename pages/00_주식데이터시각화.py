ee18401137b4133fc4680ab46dabf8361d94ce41
tthyuk <hyuk9331@gmail.com>
2025-06-10 19:07:19 +0900

    Update 00_주식데이터시각화.py

--- pages/00_주식데이터시각화.py
+++ pages/00_주식데이터시각화.py
@@ -98,7 +98,7 @@ if not stock_data.empty:
         # 마우스 오버 시 모든 라인 정보 표시
         fig_normalized.update_layout(
             hovermode="x unified",
-            # height=600, # 차트 높이 증가 설정 제거
+            height=600, # 차트 높이 증가
             legend=dict(
                 orientation="v", # 범례를 수직으로 정렬
                 xanchor="left",  # x축 앵커를 왼쪽으로 설정
@@ -123,7 +123,7 @@ if not stock_data.empty:
         )
         fig_raw.update_layout(
             hovermode="x unified",
-            # height=600, # 차트 높이 증가 설정 제거
+            height=600, # 차트 높이 증가
             legend=dict(
                 orientation="v", # 범례를 수직으로 정렬
                 xanchor="left",  # x축 앵커를 왼쪽으로 설정
@@ -150,14 +150,12 @@ if not stock_data.empty:
                 individual_df = pd.DataFrame(close_prices[selected_company_name])
                 
                 # plotly.express.line을 사용하여 개별 기업 차트 생성
-                # trendline="ols"를 추가하여 추세선 표현
                 fig_individual = px.line(
                     individual_df,
                     x=individual_df.index,
                     y=individual_df.columns[0], # 단일 컬럼의 이름 (기업명)
                     title=f"{selected_company_name} 주가",
                     labels={"value": "종가", "index": "날짜"},
-                    trendline="ols" # OLS(최소제곱법) 기반 추세선 추가
                 )
                 fig_individual.update_layout(hovermode="x unified")
                 st.plotly_chart(fig_individual, use_container_width=True)
